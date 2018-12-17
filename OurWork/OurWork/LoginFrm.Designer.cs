54d722d6befe3d27b1707da3bc4047a5ab9daea6
321923502 <321923502@qq.com>
2018-12-17 20:37:55 +0800

    1

--- OurWork/OurWork/LoginFrm.Designer.cs
+++ OurWork/OurWork/LoginFrm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // LoginFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(612, 388);
+            this.Name = "LoginFrm";
             this.Text = "LoginFrm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
