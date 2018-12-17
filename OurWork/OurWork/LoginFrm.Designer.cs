d1299451a6772b891ba97ce020fc81d719cca684
321923502 <321923502@qq.com>
2018-12-17 20:39:37 +0800

    框架及一些简单代码

--- OurWork/OurWork/LoginFrm.Designer.cs
+++ OurWork/OurWork/LoginFrm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(532, 353);
+            this.ClientSize = new System.Drawing.Size(568, 366);
             this.Name = "LoginFrm";
             this.Text = "LoginFrm";
             this.ResumeLayout(false);
