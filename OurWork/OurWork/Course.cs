6f3ec4ed8659e8e6f451ca73a38031f2bbd49682
321923502 <321923502@qq.com>
2018-12-17 20:32:54 +0800

    123

---
+++ OurWork/OurWork/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurWork
+{
+    class Course
+    {
+        public string name { set; get; }
+        public string teacher { set; get; }
+        public int fromWeek { set; get; }
+        public int endWeek { set; get; }
+        public int fromClass { set; get; }
+        public int endClass { set; get; }
+        public int day { set; get; }
+        public string area { set; get; }
+        public string classroom { set; get; }
+        public int interval { set; get; }
+        public string note { set; get; }
+        public string detail { set; get; }
+    }
+}
